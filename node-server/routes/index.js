8e8c8a951fa4d421a7ea94ad478d1316929a4521
Dennis Orbison <orbison.dennis@gmail.com>
2019-08-26 02:06:48 -0700

    basic set up

--- node-server/routes/index.js
+++ node-server/routes/index.js
@@ -1,1 +1,59 @@
+
+const playerList = [];
+const playerPointer = 0
+const minerList = [];
+let currentLastProof, prevLastProof, difficulty;
+
+const getLastProof = (player_id) => {
+  // request proof from server
+  let url = "none"
+  axios.get(url)
+  .then( results => {
+    if (results.proof !== currentLastProof) {
+      prevLastProof = currentLastProof
+      currentLastProof = results.proof
+      difficulty = results.difficulty
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+const manageLastProofRequest = () => {
+  // invoke this function every 10 seconds
+  if (playerList.length > 0) {
+    getLastProof(playerList[playerPointer++])
+    playerPointer = playerPointer % playerList.length
+  }
+}
+
+
+const addPlayer = async (req, res) => {
+  // post
+  const { player_id } = req.body
+  playerList.push(player_id)
+  res.send("player id added to player list");
+};
+
+const getMinerID = async (req, res) => {
+  // get
+  const minerId = minerList.length
+  minerList.push(minerId)
+  res.send(minerId);
+};
+
+const mineLastProof = async (req, res) => {
+  // get last proof from server
+  // get based on timer
+  res.send("sanity check");
+};
+
+// const getLastProof = async (req, res) => {
+//   // get last proof from server
+//   // get based on timer
+//   res.send("sanity check");
+// };
+
+
 module.exports = {};
